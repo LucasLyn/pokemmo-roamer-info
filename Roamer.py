5e620608222fa503ccb18ab62703166e7bd1ba71
LucasLyn <lucaslyn1@gmail.com>
2024-06-07 23:53:24 +0200

    Added documentation andrRenamed 'is_currently_available()' to 'is_available()'

--- Roamer.py
+++ Roamer.py
@@ -13,15 +13,36 @@ class Roamer:
         self.avail_locations = avail_locations
     
 
-    def is_currently_available(self,
-                               current_month:int
-                               ) -> bool:
-        return current_month in self.avail_months
+    def is_available(self,
+                     month:int
+                     ) -> bool:
+        '''
+        Checks if the roamer is available in a given month.
+
+        ### Parameters:
+        - month (int):
+            The given month to check availability for.
+        
+        ### Returns:
+            A bool corresponding to whether or not the roamer is available in the given
+        '''
+        return month in self.avail_months
     
 
     def get_avail_months(self,
                          months:list[str]
                          )-> str:
+        '''
+        Gets a string of the name of months the roamer is available in.
+
+        ### Parameters:
+        - months (list):
+            All month names to pick from.
+        
+        ### Returns:
+            A string with all the month names the roamer is available in.
+            String format: MONTH_NAME1, MONTH_NAME2, ..., MONTH_NAMEn
+        '''
         ret_str = ''
 
         for month in self.avail_months:
@@ -34,6 +55,13 @@ class Roamer:
     
 
     def get_avail_locations(self)-> str:
+        '''
+        Gets a string of locations where the roamer can be found.
+
+        ### Returns:
+            A string with all the locations the roamer can be found in.
+            String format: LOC1, LOC2, ..., LOCn
+        '''
         ret_str = ''
 
         for location in self.avail_locations:
@@ -45,12 +73,10 @@ class Roamer:
         return ret_str
 
 
-    def __str__(self
-                ) -> str:
-
+    def __str__(self) -> str:
         return ''.join([
                f"## {self.name}:\n",
                f"Region: {self.region}\n",
                f"Can be found outdoors in: {self.get_avail_locations()}\n",
                f"Available months: {self.get_avail_months(month_names)}\n",
-               f"Currently available: {self.is_currently_available(current_month)}\n"])
+               f"Currently available: {self.is_available(current_month)}\n"])
