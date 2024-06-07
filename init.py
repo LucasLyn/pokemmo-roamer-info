b3b1251ab5eb2d1a322ff6a918a502687c2591f3
LucasLyn <lucaslyn1@gmail.com>
2024-06-07 17:43:18 +0200

    Created init file to get current date data

---
+++ init.py
@@ -0,0 +1,14 @@
+from datetime import datetime # Used for dates
+import pytz # Used for timezone
+import calendar # Used to get proper date strings (month, day, etc)
+
+# Save month names list in variable for easier reference
+month_names = calendar.month_name
+
+# PokeMMO is based on UTC
+timezone = pytz.UTC
+
+# Get the current month
+current_time = datetime.now(timezone)
+current_month = current_time.month
+current_month_name = month_names[current_month]
